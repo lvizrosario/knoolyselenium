589f1ec320cc4b0f0bbb04d18242bcef927926a0
Luiz Carlos Araujo <lucafi.araujo@gmail.com>
2022-05-25 10:38:45 -0300

    Validação Linha de Negócio - Finalizado

--- src/test/java/negocio/TesteLinhaDeNegocioPage.java
+++ src/test/java/negocio/TesteLinhaDeNegocioPage.java
@@ -4,10 +4,6 @@ import dsl.DSL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 
 public class TesteLinhaDeNegocioPage {
