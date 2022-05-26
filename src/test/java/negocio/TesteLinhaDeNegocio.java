89da6f803cc90c1a2937fcfd6da45f4526141309
Luiz Carlos Araujo <lucafi.araujo@gmail.com>
2022-05-26 01:17:46 -0300

    Exclusão Linha de Negócio - Finalizado

--- src/test/java/negocio/TesteLinhaDeNegocio.java
+++ src/test/java/negocio/TesteLinhaDeNegocio.java
@@ -48,11 +48,41 @@ public class TesteLinhaDeNegocio {
         Assert.assertEquals("Luiz Filho", testeLoginPage.obterNomeUsuarioLogado());
 
         // Cadastrando uma nova linha de negócio
-        testeLinhaDeNegocioPage.adicionarLinhaDeNegocio();
+        testeLinhaDeNegocioPage.adicionarLinhaDeNegocio("Teste Luiz 4");
 
         // Validando a mensagem de cadastro realizado com sucesso
-        Assert.assertEquals("Salvo com sucesso", testeLinhaDeNegocioPage.getMensagemSucesso());
+        Assert.assertEquals("Salvo com sucesso", testeLinhaDeNegocioPage.getMensagemSucesso("//span[normalize-space()='Salvo com sucesso']"));
+    }
 
+    @Test
+    public void excluirLinhaDeNegocio() {
+        TesteLoginPage testeLoginPage = new TesteLoginPage();
+
+        // Efetuando o login e suas respectivas validações
+        testeLoginPage.preencherformularioLogin("luiz.filho@keeggo.com", "Knooly123");
+        this.testeLinhaDeNegocioPage = testeLoginPage.efetuarLogin();
+
+        Assert.assertEquals("luiz.filho@keeggo.com", testeLoginPage.obterEmailLogin());
+        Assert.assertEquals("Knooly123", testeLoginPage.obterPasswordLogin());
+
+        // Aguardando 3seg para que o botao da pagina de validação seja apresentado
+        testeLoginPage.aguardar(testeLoginPage.botaoValidarCodigo());
+        testeLoginPage.setToken("654321");
+        testeLoginPage.validarToken();
+
+        // Validando a URL atual e o token
+        Assert.assertEquals("https://knooly-qa.azurewebsites.net/#/verification-code", testeLoginPage.urlAtual());
+        Assert.assertEquals("654321", testeLoginPage.obterToken());
+
+        // Validando a tela inicial pelo nome do usuário logado
+        testeLoginPage.aguardar(testeLoginPage.obterIdUsuarioLogado());
+        Assert.assertEquals("Luiz Filho", testeLoginPage.obterNomeUsuarioLogado());
+
+        // Excluindo linha de negócio
+        testeLinhaDeNegocioPage.excluirLinhaDeNegocio("line-of-business-1");
+
+        // Validando a mensagem de exclusão realizada com sucesso
+        Assert.assertEquals("Excluído com sucesso", testeLinhaDeNegocioPage.getMensagemSucesso("//span[normalize-space()='Excluído com sucesso']"));
     }
 
 }
--- src/test/java/negocio/TesteLinhaDeNegocioPage.java
+++ src/test/java/negocio/TesteLinhaDeNegocioPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 
+
 public class TesteLinhaDeNegocioPage {
 
     private final WebDriver browser;
@@ -20,23 +21,36 @@ public class TesteLinhaDeNegocioPage {
         this.browser.quit();
     }
 
-    public void adicionarLinhaDeNegocio() {
+    public String getMensagemSucesso(String xPath) {
+        try {
+            dsl.aguardarBy(By.xpath(xPath));
+            return browser.findElement(By.xpath(xPath)).getText();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    public void adicionarLinhaDeNegocio(String nomeLinhaDeNegocio) {
         browser.findElement(By.id("btn-menu")).click();
         browser.findElement(By.id("btn-menu-0")).click();
         dsl.aguardarBy(By.id("btn-add-line-business"));
         browser.findElement(By.id("btn-add-line-business")).click();
         dsl.aguardarBy(By.id("mat-dialog-0"));
-        dsl.escreverTexto("txt-LineOfBusinessName", "Linha Teste Luiz 2");
+        dsl.escreverTexto("txt-LineOfBusinessName", nomeLinhaDeNegocio);
         browser.findElement(By.id("btn-save-add-line-business")).click();
     }
 
-    public String getMensagemSucesso() {
-        try {
-            dsl.aguardarBy(By.xpath("//span[normalize-space()='Salvo com sucesso']"));
-            return browser.findElement(By.xpath("//span[normalize-space()='Salvo com sucesso']")).getText();
-        } catch (NoSuchElementException e) {
-            return null;
-        }
+    public void excluirLinhaDeNegocio(String idLinhaDeNegocio) {
+        browser.findElement(By.id("btn-menu")).click();
+        browser.findElement(By.id("btn-menu-0")).click();
+        dsl.aguardarBy(By.id(idLinhaDeNegocio));
+        browser.findElement(By.id(idLinhaDeNegocio)).click();
+        dsl.aguardarBy(By.id("btn-edit-line-of-business"));
+        browser.findElement(By.id("btn-edit-line-of-business")).click();
+        dsl.aguardarBy(By.id("mat-dialog-0"));
+        browser.findElement(By.id("btn-line-of-business-delete")).click();
+        dsl.aguardarBy(By.id("mat-dialog-0"));
+        browser.findElement(By.id("btn-line-of-business-yes")).click();
     }
 
 }
